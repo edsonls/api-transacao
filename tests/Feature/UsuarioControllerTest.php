2464611d509930bb72c1a5598a009a0fb3c93e71
edson.junior <edson.junior@sinqia.com.br>
2021-09-29 20:19:47 -0300

    Classe para testar Controller de usuario

---
+++ tests/Feature/UsuarioControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+
+use App\Controllers\UsuarioController;
+use App\Entities\Enum\TipoUsuarioEnum;
+use GuzzleHttp\Psr7\Utils;
+
+beforeEach(
+  function () {
+    $this->fake = Faker\Factory::create('pt_BR');
+    $this->usuarioController = new UsuarioController();
+  }
+);
+it(
+  'OK - Criando Usuario Comum',
+  function () {
+    expect(
+      $this->usuarioController->add(
+        Utils::streamFor(
+          json_encode([
+                        'nome' => $this->fake->name,
+                        'documento' => $this->fake->cpf,
+                        'email' => $this->fake->email,
+                        'senha' => $this->fake->password,
+                        'tipoUsuario' => TipoUsuarioEnum::Comum,
+                        'saldo' => 50,
+                      ])
+        )
+      )
+    )
+      ->toBeInt();
+  }
+);
+it(
+  'OK - Criando Usuario Logista',
+  function () {
+    expect(
+      $this->usuarioController->add(
+        Utils::streamFor(
+          json_encode([
+                        'nome' => $this->fake->name,
+                        'documento' => $this->fake->cpf,
+                        'email' => $this->fake->email,
+                        'senha' => $this->fake->password,
+                        'tipoUsuario' => TipoUsuarioEnum::Logista,
+                        'saldo' => 50,
+                      ])
+        )
+      )
+    )
+      ->toBeInt();
+  }
+);
