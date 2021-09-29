b50b6c8f4bdfcc9654a3cc46df664e05b689763d
edson.junior <edson.junior@sinqia.com.br>
2021-09-29 20:56:54 -0300

    Refatorando os testes

--- tests/Feature/UsuarioControllerTest.php
+++ tests/Feature/UsuarioControllerTest.php
@@ -3,6 +3,7 @@
 
 use App\Controllers\UsuarioController;
 use App\Entities\Enum\TipoUsuarioEnum;
+use App\Utils\Errors\Interfaces\IError;
 use GuzzleHttp\Psr7\Utils;
 
 beforeEach(
@@ -49,5 +50,114 @@ it(
       )
     )
       ->toBeInt();
+  }
+);
+
+it(
+  'OK - Criando Usuario Comum Sem saldo',
+  function () {
+    expect(
+      $this->usuarioController->add(
+        Utils::streamFor(
+          json_encode([
+                        'nome' => $this->fake->name,
+                        'documento' => $this->fake->cpf(false),
+                        'email' => $this->fake->email,
+                        'senha' => $this->fake->password,
+                        'tipoUsuario' => TipoUsuarioEnum::Comum,
+                        'saldo' => 0,
+                      ])
+        )
+      )
+    )
+      ->toBeInt();
+  }
+);
+
+it(
+  'OK - Criando Usuario Logista Sem saldo',
+  function () {
+    expect(
+      $this->usuarioController->add(
+        Utils::streamFor(
+          json_encode([
+                        'nome' => $this->fake->name,
+                        'documento' => $this->fake->cpf(false),
+                        'email' => $this->fake->email,
+                        'senha' => $this->fake->password,
+                        'tipoUsuario' => TipoUsuarioEnum::Logista,
+                        'saldo' => 0,
+                      ])
+        )
+      )
+    )
+      ->toBeInt();
+  }
+);
+
+it(
+  'Fail - Criando Usuario Comum Documento Repetido',
+  function () {
+    $documento = $this->fake->cpf(false);
+    $this->usuarioController->add(
+      Utils::streamFor(
+        json_encode([
+                      'nome' => $this->fake->name,
+                      'documento' => $documento,
+                      'email' => $this->fake->email,
+                      'senha' => $this->fake->password,
+                      'tipoUsuario' => TipoUsuarioEnum::Comum,
+                      'saldo' => 0,
+                    ])
+      )
+    );
+    expect(
+      $this->usuarioController->add(
+        Utils::streamFor(
+          json_encode([
+                        'nome' => $this->fake->name,
+                        'documento' => $documento,
+                        'email' => $this->fake->email,
+                        'senha' => $this->fake->password,
+                        'tipoUsuario' => TipoUsuarioEnum::Comum,
+                        'saldo' => 0,
+                      ])
+        )
+      )
+    )
+      ->toBeInstanceOf(IError::class);
+  }
+);
+it(
+  'Fail - Criando Usuario Logista Documento Repetido',
+  function () {
+    $documento = $this->fake->cpf(false);
+    $this->usuarioController->add(
+      Utils::streamFor(
+        json_encode([
+                      'nome' => $this->fake->name,
+                      'documento' => $documento,
+                      'email' => $this->fake->email,
+                      'senha' => $this->fake->password,
+                      'tipoUsuario' => TipoUsuarioEnum::Logista,
+                      'saldo' => 0,
+                    ])
+      )
+    );
+    expect(
+      $this->usuarioController->add(
+        Utils::streamFor(
+          json_encode([
+                        'nome' => $this->fake->name,
+                        'documento' => $documento,
+                        'email' => $this->fake->email,
+                        'senha' => $this->fake->password,
+                        'tipoUsuario' => TipoUsuarioEnum::Logista,
+                        'saldo' => 0,
+                      ])
+        )
+      )
+    )
+      ->toBeInstanceOf(IError::class);
   }
 );
